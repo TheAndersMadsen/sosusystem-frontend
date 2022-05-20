06de8c25f95ff278e65c944b04a434f2041b7bd4
Nikolai Gorzelak Pedersen <71375600+Niko4730@users.noreply.github.com>
2022-05-20 11:21:56 +0200

    Fontawesome deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CitizenGeneralInfoComponent } from './citizen-general-info/citizen-general-info.component';
 import { CitizenHealthConditionsComponent } from './citizen-health-conditions/citizen-health-conditions.component';
 import { FooterComponent } from './footer/footer.component';
@@ -37,7 +35,6 @@ import { SubcategoryModalFunctionComponent } from './subcategory-modal-function/
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-      FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
